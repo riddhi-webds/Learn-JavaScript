91d35b6f83d1a19fd27fd7623b3603f23995e1b4
riddhi-webds <100923091+riddhi-webds@users.noreply.github.com>
2023-10-06 07:10:52 +0000

    datatype done

---
+++ 01_basics/datType_summary.js
@@ -0,0 +1,15 @@
+// primitive
+
+// 7 type: string, boolean, number, null, undefined, symbol, BigInt
+
+// Reference (Non-primitive)
+
+//Arrays, objects, functions
+
+//++++++++++++++++++++++++++++++++++++++++
+
+// two types of memory use with dayatypes
+
+//  stack (primitive data(copy data use)), heap(non-primitive data(original data use))
+
+//
